ac866af7a52c7941ce154006244848dac152b743
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2020-05-09 21:34:56 -0400

    Back on the working system

--- src/javascript/search_engine.js
+++ src/javascript/search_engine.js
@@ -1,72 +1,50 @@
-console.log("Search engine script works!")
-
-function saveCookies() {
-    myCookie["_uLastSearchEngine"] = sengine.options[sengine.selectedIndex].text;
-    document.cookie = "";
-    var expiresAttrib = new Date().setFullYear(new Date().getFullYear() + 1).toString();
-    var cookieString = "";
-    for (var key in myCookie) {
-        cookieString = key + "=" + myCookie[key] + ";" + expiresAttrib + ";";
-        document.cookie = cookieString;
-    }
+switch (document.cookie) {
+    case "sengine=Bing":
+        document.getElementById("bing").selected = "selected";
+        break;
+    case "sengine=DuckDuckGo":
+        document.getElementById("ddgo").selected = "selected";
+        break;
+    case "sengine=Ecosia":
+        document.getElementById("ecosia").selected = "selected";
+        break;
+    case "sengine=Facebook":
+        document.getElementById("fb").selected = "selected";
+        break;
+    case "sengine=GitHub":
+        document.getElementById("gh").selected = "selected";
+        break;
+    case "sengine=Google":
+        document.getElementById("google").selected = "selected";
+        break;
+    case "sengine=Qwant":
+        document.getElementById("qwant").selected = "selected";
+        break;
+    case "sengine=Youtube":
+        document.getElementById("yt").selected = "selected";
+        break;
+    default:
+        document.getElementById("google").selected = "selected";
 }
 
-function loadCookies() {
-    myCookie = {};
-    var key_value = document.cookie.split(";");
-    for (var id in key_value) {
-        var cookie = key_value[id].split("=");
-        myCookie[cookie[0].trim()] = cookie[1];
-    }
-
-    // Get the last used search engine
-    select_last_search_engine(myCookie["_uLastSearchEngine"]);
-}
-
-function select_last_search_engine(lastSearchEngine) {
-    switch (lastSearchEngine) {
-        case "Bing":
-            document.getElementById("bing").selected = "selected";
-            break;
-        case "DuckDuckGo":
-            document.getElementById("ddgo").selected = "selected";
-            break;
-        case "Ecosia":
-            document.getElementById("ecosia").selected = "selected";
-            break;
-        case "Facebook":
-            document.getElementById("fb").selected = "selected";
-            break;
-        case "GitHub":
-            document.getElementById("gh").selected = "selected";
-            break;
-        case "Google":
-            document.getElementById("google").selected = "selected";
-            break;
-        case "Qwant":
-            document.getElementById("qwant").selected = "selected";
-            break;
-        case "Youtube":
-            document.getElementById("yt").selected = "selected";
-            break;
-        default:
-            document.getElementById("google").selected = "selected";
-            break;
-    }
-    set_sengine_link();
+function get_expiration_date() {
+    var date = new Date();
+    var year = date.getFullYear() + 1;
+    return new Date(year, date.getMonth(), date.getDate());
 }
 
 function set_sengine_link() {
     var sengine = document.getElementById("sengine"); // getting the selected web search engine
+    var selected_sengine = sengine.options[sengine.selectedIndex].text;
 
-    switch (sengine.options[sengine.selectedIndex].text) {
+    switch (selected_sengine) {
         case "Bing":
             document.getElementById("SENGINE").action = "https://www.bing.com/search";
             break;
         case "DuckDuckGo":
             document.getElementById("SENGINE").action = "https://duckduckgo.com/";
             break;
-        case "Ecosia": 
+        case "Ecosia":
             document.getElementById("SENGINE").action = "https://www.ecosia.org/search";
             break;
         case "Facebook":
@@ -75,10 +53,10 @@ function set_sengine_link() {
         case "GitHub":
             document.getElementById("SENGINE").action = "https://github.com/search";
             break;
-        case "Google": 
+        case "Google":
             document.getElementById("SENGINE").action = "https://www.google.com/search";
             break;
-        case "Qwant": 
+        case "Qwant":
             document.getElementById("SENGINE").action = "https://www.qwant.com/";
             break;
         case "Youtube":
@@ -87,7 +65,8 @@ function set_sengine_link() {
         default:
             document.getElementById("SENGINE").action = "https://www.google.com/search";
     }
-    saveCookies();
+    document.cookie = `sengine=${selected_sengine};` + `expires=${get_expiration_date()}`;
+    console.log(`sengine=${selected_sengine};` + `expires=${get_expiration_date()}`)
 }
 
-loadCookies();
+set_sengine_link();
